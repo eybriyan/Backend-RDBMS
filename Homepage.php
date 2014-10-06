c3556201e8bc7065aefacdbd2dc04311b2bfe6d6
JakeyPayat <jakeymores@gmail.com>
2014-10-06 21:55:53 +0800

    Revert "Revert "Homepage Table Edit""
    This reverts commit bc150251f6affdaf10a234d5c271ed174b8d0a58.

--- Homepage.php
+++ Homepage.php
@@ -1,8 +1,8 @@
+
 <html>
-<?php session_start();?>
 <head>
 	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
 </head>
 <style>
 .header1 {
@@ -232,16 +232,18 @@
 		echo file_get_contents("index.php");
 		echo "<div style = 'font-size:24'>Please Log-IN</div>";
 	}
+
 } 
 function ViewClient() {
     $viewquery = mysql_query("SELECT * from tbl_client") or die(mysql_error());
-	echo '<center><p class = "header1"> View Client</p></center>
-	<div class= "TableID">
-	<table border="0" cellpadding="10">
+	echo '<p> View Client</p>
+	<div class = 'TableID'>
+	<table border="1" cellpadding="10" TableID>
 	 <tr>
-	 <th>ClientName</th>
-	 <th>Client Contact Person</th> 
-	 <th>Client Address</th>
+	 <td>ClientName</td>
+	 <td>Client Contact Person</td> 
+	 <td>Client Address</td>
+	 <td>Properties</td>
 	 </tr>';
 	  while($row = mysql_fetch_array( $viewquery )) {
 		echo '<tr>
@@ -251,74 +253,68 @@ function ViewClient() {
 		<td>
 		<center>
 		<form method = "post" action="edit_client.php?client_id=' . $row['client_id'] . '">
-			<input type = "submit" name= "submitfromhome" value = "Edit" class ="mybutton">
+			<input type = "submit" name= "submitfromhome" value = "Edit" class = "myButton">
 		</form>
-		</center>
-		</td>
-		<td>
-		<center>
+		
 		<form method = "post" action ="delete.php?client_id=' . $row['client_id'] . '">
-			<input type = "submit" name = "delsubmithome" value = "Delete" class="mybutton">
+			<input type = "submit" name = "delsubmithome" value = "Delete" class = "myButton">
 		</form>
 		</center>
 		</td>
 		</tr>';
 	} 
       echo '</table>
-      <br>
+      </div>
+      
       <div>
-      <center>
       <form method = "post" action ="add_client.php">
-			<input type = "submit" name = "addsubmithome" value = "Add New Record" class="mybutton1">
+			<input type = "submit" name = "addsubmithome" value = "Add New Record" class= "myButton1">
 	  </form>
-	  </center>
 	  </div>';
 
 } 	
 function ViewSupplier(){
 	$viewquery = mysql_query("SELECT * from tbl_supplier") or die(mysql_error());
-	echo '<center><p class = "header1"> View Supplier</p></center>
-	<div class="TableID">
-	 <table border="0" cellpadding="10">
+	echo '<p> View Supplier</p>
+	<div class = 'TableID'>
+	 <table border="1" cellpadding="10">
 	   <tr>
-	   <th>Supplier Name</th>
-	   <th>Supplier Contact Person</th>
-	   <th>Supplier Address</th>
+	   <td>Supplier Name</td>
+	   <td>Supplier Contact Person</td>
+	   <td>Supplier Address</td>
+	   <td>Properties</td>
 	   </tr>';
 	  while($row = mysql_fetch_array( $viewquery )) {
 		
 		echo '<tr>
-		<td><a href="supplier_transaction.php?supplier_id=' . $row['supplier_id']. '">' . $row['supplier_name'] . '</a></td>
+		<td><a href="view_supplier.php?supplier_id=' . $row['supplier_id']. '">' . $row['supplier_name'] . '</a></td>
 		<td>' . $row['supplier_contactperson'] . '</td>
 		<td>' . $row['supplier_address'] . '</td>
 		<td>
 		<center>
 		<form method = "post" action="edit_supplier.php?supplier_id=' . $row['supplier_id'] . '">
-			<input type = "submit" name= "submitfromhome" value = "Edit" class="mybutton">
+			<input type = "submit" name= "submitfromhome" value = "Edit" class = "myButton">
 		</form>
-		</center>
-		</td>
-		<td>
-		<center>
+		
 		<form method = "post" action ="delete.php?supplier_id=' . $row['supplier_id'] . '">
-			<input type = "submit" name = "delsubmithome" value = "Delete" class="mybutton">
+			<input type = "submit" name = "delsubmithome" value = "Delete"  class = "myButton">
 		</form>
 		</center>
 		</td>
 		</tr>';
 	} 
       echo '</table>
-      <br>
+      </div>
+      
       <div>
-      <center>
       <form method = "post" action ="add_supplier.php">
-			<input type = "submit" name = "addsubmithome" value = "Add New Record" class="mybutton1">
+			<input type = "submit" name = "addsubmithome" value = "Add New Record" class= "myButton1">
 	  </form>
-	  </center>
 	  </div>';
+</center>
 }
 ?>
-</div>
+</head>
+<body>
 </body>
 </html>
-
